a7c9c00d031bd687d7915339ed594b370dc68110
rafaelcbaptista <rafaelcbaptista@gmail.com>
2022-03-12 23:31:07 -0300

    first commit

---
+++ src/main/java/br/com/fiap/abctechservice/controller/HealthCheckController.java
@@ -0,0 +1,19 @@
+package br.com.fiap.abctechservice.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/")
+
+
+public class HealthCheckController {
+
+    @GetMapping()
+    public ResponseEntity<String> status(){
+
+        return ResponseEntity.ok("Sucesso !");
+    }
+}
